d0e032d4c55f07c8b5beb9561a75c3b50c1eb03c
MouseProducedGames <largemouse@gmail.com>
2020-10-09 23:24:46 -0700

    Add: Helper impls for ProvidesSize.

--- src/provides_size.rs
+++ src/provides_size.rs
@@ -12,3 +12,32 @@ pub trait ProvidesSize {
     /// Provides a `Size` when called.
     fn provide_size(&self) -> Size;
 }
+
+impl<TProvidesSize> ProvidesSize for Box<TProvidesSize>
+where
+    TProvidesSize: ProvidesSize,
+{
+    fn provide_size(&self) -> Size {
+        (**self).provide_size()
+    }
+}
+
+struct SizeProvider<TFunc>
+where
+    TFunc: Fn() -> Size,
+{
+    func: TFunc,
+}
+
+impl<TFunc> ProvidesSize for SizeProvider<TFunc>
+where
+    TFunc: Fn() -> Size,
+{
+    fn provide_size(&self) -> Size {
+        (self.func)()
+    }
+}
+
+fn size_provider<TFunc: Fn() -> Size>(t_func: TFunc) -> SizeProvider<TFunc> {
+    SizeProvider { func: t_func }
+}
