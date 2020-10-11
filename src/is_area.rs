6a2b467fb8b03e7e3c557b0f6e54e56943651871
MouseProducedGames <largemouse@gmail.com>
2020-10-10 20:11:48 -0700

    Add: Functions to calculate the right and bottom positions/shape positions of an IsArea, and an IsShapeArea.

--- src/is_area.rs
+++ src/is_area.rs
@@ -3,7 +3,21 @@
 // Standard includes.
 
 // Internal includes.
-use super::{HasArea, IsSize, ProvidesArea};
+use super::{Coord, HasArea, IsPosition, IsSize, ProvidesArea};
 
 /// `IsArea` is defined as both [`HasPosition`](trait.HasPosition.html), [`HasSize`](trait.HasSize.html), and [`IsSize`](trait.IsSize.html).
-pub trait IsArea: HasArea + IsSize + ProvidesArea {}
+pub trait IsArea: HasArea + IsSize + ProvidesArea {
+    /// Obtains the right-most coordinate of the area.
+    ///
+    /// A geometic tile area with a width of 1, has the same right tile as its left tile.
+    fn right(&self) -> Coord {
+        self.position().x() + (self.width() as Coord - 1).max(0)
+    }
+
+    /// Obtains the right-most coordinate of the area.
+    ///
+    /// A geometic tile area with a height of 1, has the same bottom tile as its top tile.
+    fn bottom(&self) -> Coord {
+        self.position().y() + (self.height() as Coord - 1).max(1)
+    }
+}
--- src/is_shape_area.rs
+++ src/is_shape_area.rs
@@ -3,7 +3,21 @@
 // Standard includes.
 
 // Internal includes.
-use super::{HasShapeArea, IsSize, ProvidesShapeArea};
+use super::{Coord, HasShapeArea, IsShapePosition, IsSize, ProvidesShapeArea};
 
 /// `IsArea` is defined as both [`HasShapeArea`](trait.HasShapeArea.html), [`IsSize`](trait.IsSize.html), and [`ProvidesShapeArea`](trait.ProvidesShapeArea.html).
-pub trait IsShapeArea: HasShapeArea + IsSize + ProvidesShapeArea {}
+pub trait IsShapeArea: HasShapeArea + IsSize + ProvidesShapeArea {
+    /// Obtains the right-most coordinate of the shape area.
+    ///
+    /// A geometic tile area with a width of 1, has the same right tile as its left tile.
+    fn right(&self) -> Coord {
+        self.shape_position().x() + (self.width() as Coord - 1).max(0)
+    }
+
+    /// Obtains the right-most coordinate of the shape area.
+    ///
+    /// A geometic tile area with a height of 1, has the same bottom tile as its top tile.
+    fn bottom(&self) -> Coord {
+        self.shape_position().y() + (self.height() as Coord - 1).max(1)
+    }
+}
