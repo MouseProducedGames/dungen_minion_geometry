6306517fd7995bd0e8c11809b68b359c91251875
MouseProducedGames <largemouse@gmail.com>
2020-10-20 15:32:59 -0700

    Add: Accessors and mutable accessors for the left and top of an area; setters for the right and bottom of an area.

--- src/is_area.rs
+++ src/is_area.rs
@@ -3,21 +3,67 @@
 // Standard includes.
 
 // Internal includes.
-use super::{Coord, HasArea, IsPosition, IsSize, ProvidesArea};
+use super::{Coord, HasArea, IsPosition, IsSize, Length, ProvidesArea};
 
 /// `IsArea` is defined as both [`HasPosition`](trait.HasPosition.html), [`HasSize`](trait.HasSize.html), and [`IsSize`](trait.IsSize.html).
 pub trait IsArea: HasArea + IsSize + ProvidesArea {
-    /// Obtains the right-most coordinate of the area.
+    /// The left-most coordinate of the area.
+    ///
+    /// Horizontal coordinates increase towards the east.
+    fn left(&self) -> Coord {
+        self.position().x()
+    }
+
+    /// A mutable reference to the left-most coordinate of the area.
+    ///
+    /// Horizontal coordinates increase towards the east.
+    fn left_mut(&mut self) -> &mut Coord {
+        self.position_mut().x_mut()
+    }
+
+    /// The top-most coordinate of the area.
+    ///
+    /// Vertical coordinates increase towards the south.
+    fn top(&self) -> Coord {
+        self.position().y()
+    }
+
+    /// A mutable reference to the top-most coordinate of the area.
+    ///
+    /// Vertical coordinates increase towards the south.
+    fn top_mut(&mut self) -> &mut Coord {
+        self.position_mut().y_mut()
+    }
+
+    /// The right-most coordinate of the area.
     ///
     /// A geometic tile area with a width of 1, has the same right tile as its left tile.
     fn right(&self) -> Coord {
         self.position().x() + (self.width() as Coord - 1).max(0)
     }
 
-    /// Obtains the right-most coordinate of the area.
+    /// Sets the right-most coordinate of the area.
+    ///
+    /// Cannot set the right-most coordinate to less than the x-coordinate.
+    fn right_set(&mut self, value: Coord) {
+        let width_coord = (value - self.position().x()) + 1;
+        let width = width_coord.max(0) as Length;
+        *self.width_mut() = width;
+    }
+
+    /// The right-most coordinate of the area.
     ///
     /// A geometic tile area with a height of 1, has the same bottom tile as its top tile.
     fn bottom(&self) -> Coord {
         self.position().y() + (self.height() as Coord - 1).max(1)
+    }
+
+    /// Sets the bottom-most coordinate of the area.
+    ///
+    /// Cannot set the bottom-most coordinate to less than the y-coordinate.
+    fn bottom_set(&mut self, value: Coord) {
+        let height_coord = (value - self.position().y()) + 1;
+        let height = height_coord.max(0) as Length;
+        *self.height_mut() = height;
     }
 }
