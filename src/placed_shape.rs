529f1aaea3e692ae4f6cdfa016664f05b80874b2
MouseProducedGames <largemouse@gmail.com>
2020-10-21 12:50:57 -0700

    Change: Simplified the implementation of PlacedShape.

--- src/placed_shape.rs
+++ src/placed_shape.rs
@@ -9,5 +9,15 @@ use super::{IntersectsPos, PlacedObject, Shape};
 ///
 /// "shape" is used instead of "polygon" as shapes do not necessarily have regular vertices or edges, nor do they necessarily conform to any regular geometric definition.
 pub trait PlacedShape: IntersectsPos + PlacedObject + Shape {
+    /// Returns this `PlacedShape` as a `&dyn Shape`.
     fn as_shape(&self) -> &dyn Shape;
+}
+
+impl<TShape> PlacedShape for TShape
+where
+    TShape: IntersectsPos + PlacedObject + Shape,
+{
+    fn as_shape(&self) -> &dyn Shape {
+        self
+    }
 }
