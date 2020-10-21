69b345c06cfddd28e304f60fdcdb2ee15ba53621
MouseProducedGames <largemouse@gmail.com>
2020-10-21 12:34:14 -0700

    Update: Reconfigured IntersectsPlacedShape and IntersectsShape to be non-generic for usability and trait objects.

--- src/intersects_placed_shape.rs
+++ src/intersects_placed_shape.rs
@@ -6,10 +6,7 @@
 use super::PlacedShape;
 
 /// Defines an intersection operation between this instance and a [`PlacedShape`](trait.PlacedShape.html).
-pub trait IntersectsPlacedShape<TOtherPlacedShape>
-where
-    TOtherPlacedShape: PlacedShape,
-{
-    /// Returns true if the given 'PlacedShape' at the given offset is within the boundaries of this instance.
-    fn intersects(&self, other_placed_shape: &TOtherPlacedShape) -> bool;
+pub trait IntersectsPlacedShape {
+    /// Returns true if the given 'PlacedShape' at is within the boundaries of this instance.
+    fn intersects_placed_shape(&self, placed_shape: &dyn PlacedShape) -> bool;
 }
--- src/intersects_shape.rs
+++ src/intersects_shape.rs
@@ -6,10 +6,7 @@
 use super::{Position, Shape};
 
 /// Defines an intersection operation between this instance and a [`Shape`](trait.Shape.html) at a specific [`Position`](struct.Position.html).
-pub trait IntersectsShape<TOtherShape>
-where
-    TOtherShape: Shape,
-{
+pub trait IntersectsShape {
     /// Returns true if the given 'Shape' at the given `Position` offset is within the boundaries of this instance.
-    fn intersects_local(&self, other_offset: Position, other_shape: &TOtherShape) -> bool;
+    fn intersects_shape(&self, other_offset: Position, other_shape: &dyn Shape) -> bool;
 }
